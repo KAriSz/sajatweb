739d8646066f300d6145bfc017eb6b2e2a014794
KAriSz <molnar4435@gmail.com>
2021-06-02 21:48:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-lfqms5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-lfqms5)
---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pöcsöm öcsse</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+
+
+
+<?php
+      
+      include_once 'header.php'
+  ?>
+  
+  <section class="signup-form">
+      <h2>Sign Up</h2>
+      <div class="signup-form-form">
+      <form action="includes/signup.inc.php" method="post">
+      <div>
+      <form action="include/login.inc.php" method="post">
+      <label>Full Name</label>
+      </div>
+          <input type="text" name="name" placeholder="Full name...">
+          <div>
+        <label>Email</label>
+        </div>
+          <input type="text" name="name" placeholder="Email...">
+          <div>
+        <label>Username</label> 
+        </div>
+          <input type="text" name="name" placeholder="Username...">
+          <div>
+        <label>Password</label>
+        </div>
+          <input type="password" name="name" placeholder="password...">
+          <div>
+           <label>Repeat Password</label>
+</div>
+          <input type="password" name="name" placeholder="Repeat Password...">
+          
+
+          <div>
+          <input type="submit" value="Sign Up">
+          <p>Bejelentkezéshez!<a href="index2.php" katt ide! >    Katt ide</a>
+</div>
+          </body>
+  </div>
+  </section>
+  <?php
+      include_once 'footer.php'
+  ?>
+     
+ 
+    </div>
+
+  
+    </div>
+ 
+ 
+
+    
+    
+</body>
+</html>
+
